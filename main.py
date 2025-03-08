cbe1cb86cb6dcc3e8ffda5bec70df6b84a15ef55
An Huynh <86417268+anphongdoa5@users.noreply.github.com>
2025-03-08 10:53:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ from discord.ext import commands
 from discord.ext.commands import MissingPermissions
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
-# from dotenv.main import with_warn_for_invalid_lines
 import requests
 from discord.utils import get
 from discord.ext import commands
